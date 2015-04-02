84a3504c0d92264a66ff7af12162dd6498e5b924
Al Gonzalez <alg@nomscon.com>
2015-04-02 00:06:10 -0400

    adds new methods to PathUtils
    expandHomeDirTilde -> ex: converts ~/ to /Users/{userName}/
    tryGetCanonicalPath  -> returns new TryResult<String> instance

--- src/main/java/com/nomscon/lib/utils/PathUtils.java
+++ src/main/java/com/nomscon/lib/utils/PathUtils.java
@@ -1,7 +1,9 @@
 package com.nomscon.lib.utils;
 
 import com.nomscon.lib.Arg;
+import com.nomscon.lib.TryResult;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -33,6 +35,33 @@ public final class PathUtils {
         }
         
         return resultPath;
+    }
+    
+    public static String expandHomeDirTilde(String path) {
+        if (path == null || path.isEmpty() || !path.startsWith("~"))
+            return path;
+        final String userHomeDir = OSUtils.getOSInfo().getHomeDir();
+        final String homeDirWithTilde = "~" + File.separator;
+        if (path.startsWith(homeDirWithTilde)) {
+            return userHomeDir + path.substring(1);
+        }
+        return userHomeDir 
+                + File.separator 
+                + ".." 
+                + File.separator 
+                + path.substring(1);
+    }
+    
+    public TryResult<String> tryGetCanonicalPath(File file, String defaultValue) {
+        Arg.checkIsNotNull(file);
+        TryResult<String> tryResult;
+        try {
+            String path = file.getCanonicalPath();
+            tryResult = TryResult.asSuccess(path);
+        } catch (IOException ex) {
+            tryResult = TryResult.asFailure(defaultValue, ex);
+        }
+        return tryResult;
     }
 }
  
