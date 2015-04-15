c4f67ddc96928597b69faedb85ad85b0c95e4605
Al Gonzalez <alg@nomscon.com>
2015-04-14 21:52:02 -0400

    adds isEven and isOdd methods

--- src/main/java/com/nomscon/lib/utils/IntUtils.java
+++ src/main/java/com/nomscon/lib/utils/IntUtils.java
@@ -3,6 +3,17 @@ package com.nomscon.lib.utils;
 public final class IntUtils {
     private IntUtils() { /* cannot be instantiated */ }
 
+    public static boolean isEven(int value) {
+        // check first bit which is only set for odd values
+        // NOTE: can also check remainder when dividing by 2, 
+        //       so return (value % 2) == 0 
+        return (value & 1) == 0;
+    }
+
+    public static boolean isOdd(int value) {
+        return !isEven(value);
+    }
+
     public static boolean isInRange(int value, int minValue, int maxValue) {
         if (maxValue < minValue) {
             int tempLimit = minValue;
